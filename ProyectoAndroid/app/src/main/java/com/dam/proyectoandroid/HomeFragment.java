77f0b4e7dbd408eb37ac3584926cba8111f0187e
MSebas55 <calimandrucsebas@gmail.com>
2024-02-18 22:30:51 +0100

    volviendo atras

--- ProyectoAndroid/app/src/main/java/com/dam/proyectoandroid/HomeFragment.java
+++ ProyectoAndroid/app/src/main/java/com/dam/proyectoandroid/HomeFragment.java
@@ -1,6 +1,5 @@
 package com.dam.proyectoandroid;
 
-import android.content.Context;
 import android.content.Intent;
 
 import android.os.Bundle;
@@ -10,10 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.ListView;
-import android.widget.Toast;
 
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -22,7 +19,6 @@ import com.dam.proyectoandroid.Database.Constants;
 import com.dam.proyectoandroid.Database.Interfaces.CRUDInterface;
 import com.dam.proyectoandroid.Database.adapters.ProyectsAdapter;
 import com.dam.proyectoandroid.Database.model.Proyecto;
-import com.dam.proyectoandroid.Database.provisional;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -35,7 +31,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 
-public class HomeFragment extends Fragment{
+public class HomeFragment extends Fragment {
+
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -43,36 +40,24 @@ public class HomeFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        provisional p = new provisional();
-        p.getAll();
         // Inflate the layout for this fragment
-        View view = null;// = inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        //RecyclerView recyclerView = view.findViewById(R.id.historicEventsRecycler);
+        RecyclerView recyclerView = view.findViewById(R.id.historicEventsRecycler);
 
         // Obtener las matrices de cadenas de los recursos de cadena
-        /*ArrayList<String> eventNames = new ArrayList<>();
-        ArrayList<String> eventFechaFin = new ArrayList<>();*/
-        //Toast.makeText(getContext(), p.proyectos.get(0).getNombre(), Toast.LENGTH_SHORT).show();
-
-
-
-        /*for (Proyecto proyecto : p.proyectos) {
-            eventNames.add(proyecto.getNombre());
-            eventFechaFin.add(proyecto.getFechafin());
-        }*/
-
+        String[] eventNamesArray = getResources().getStringArray(R.array.historic_event_names);
+        String[] eventLocationsArray = getResources().getStringArray(R.array.historic_event_locations);
 
         // Convertir las matrices en ArrayList
-
-        //ArrayList<String> textColors = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.text_colors)));
+        ArrayList<String> eventNames = new ArrayList<>(Arrays.asList(eventNamesArray));
+        ArrayList<String> eventLocations = new ArrayList<>(Arrays.asList(eventLocationsArray));
+        ArrayList<String> textColors = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.text_colors)));
 
         // Crear y configurar el adaptador del RecyclerView
-        /*HistoricEventRVAdapter adapter = new HistoricEventRVAdapter(requireContext(), eventNames, eventFechaFin, textColors);
+        HistoricEventRVAdapter adapter = new HistoricEventRVAdapter(requireContext(), eventNames, eventLocations, textColors);
         recyclerView.setAdapter(adapter);
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));*/
-
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
         return view;
     }
 
